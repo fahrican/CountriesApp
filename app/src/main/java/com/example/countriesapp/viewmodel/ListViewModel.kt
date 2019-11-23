ca56ccfc631e20316f3956522c5de2a2d637a56f
Fahri MacBook <fahri-can@web.de>
2019-11-23 18:54:05 +0100

    Add class properties and method to fetch dummy data.

--- app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
@@ -1,6 +1,34 @@
 package com.example.countriesapp.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countriesapp.model.Country
 
 class ListViewModel : ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData = listOf(
+            Country("CountryA"),
+            Country("CountryB"),
+            Country("CountryC"),
+            Country("CountryD"),
+            Country("CountryE"),
+            Country("CountryF"),
+            Country("CountryG"),
+            Country("CountryH"),
+            Country("CountryI"),
+            Country("CountryJ")
+        )
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 }
