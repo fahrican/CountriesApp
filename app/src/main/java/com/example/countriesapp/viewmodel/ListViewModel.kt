6a53818694f184f6215cacea5aade18470c49a9c
Fahri MacBook <fahri-can@web.de>
2019-11-25 02:23:37 +0100

    Use of RxJava to fetch countries from backend.

--- app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/example/countriesapp/viewmodel/ListViewModel.kt
@@ -2,33 +2,52 @@ package com.example.countriesapp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.countriesapp.model.CountriesService
 import com.example.countriesapp.model.Country
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class ListViewModel : ViewModel() {
 
+    val countriesService = CountriesService()
+    val disposable = CompositeDisposable()
+
+    // LiveData
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError = MutableLiveData<Boolean>()
     val loading = MutableLiveData<Boolean>()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 
     fun refresh() {
         fetchCountries()
     }
 
     private fun fetchCountries() {
-        val mockData = listOf(
-            Country("CountryA"),
-            Country("CountryB"),
-            Country("CountryC"),
-            Country("CountryD"),
-            Country("CountryE"),
-            Country("CountryF"),
-            Country("CountryG"),
-            Country("CountryH"),
-            Country("CountryI"),
-            Country("CountryJ")
+        loading.value = true
+        disposable.add(
+            countriesService.getCountries()
+                .subscribeOn(Schedulers.newThread()) // processing on a separate thread
+                .observeOn(AndroidSchedulers.mainThread()) // show result on main-thread, is the thread which the user see
+                .subscribeWith(object :
+                    DisposableSingleObserver<List<Country>>() { // what we gonna do when we receive the information
+
+                    override fun onSuccess(value: List<Country>?) {
+                        countries.value = value
+                        countryLoadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        countryLoadError.value = true
+                        loading.value = false
+                    }
+                })
         )
-        countryLoadError.value = false
-        loading.value = false
-        countries.value = mockData
     }
 }
