d1f2605a6baffb517887cd62f0c4770defa37701
Fahri MacBook <fahri-can@web.de>
2019-11-30 15:08:30 +0100

    Use of dependency injection.

--- app/src/main/java/com/example/countriesapp/model/CountriesService.kt
+++ app/src/main/java/com/example/countriesapp/model/CountriesService.kt
@@ -6,16 +6,11 @@ import javax.inject.Inject
 
 class CountriesService {
 
-    private val BASE_URL = "https://raw.githubusercontent.com"
-    private val api: CountriesApi
+    @Inject
+    lateinit var api: CountriesApi
 
     init {
-        api = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create()) // transfers data from the backend to Kotlin objects
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create()) // makes Country class a Observable variable, so other variables can subscribe to it
-            .build()
-            .create(CountriesApi::class.java)
+        DaggerApiComponent.create().inject(this)
     }
 
     fun getCountries(): Single<List<Country>> { // Single emits one variable then closes
