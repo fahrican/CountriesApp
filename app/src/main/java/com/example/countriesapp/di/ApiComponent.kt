a2325dbea5e17284dcf6c8a9e727ce877944d29e
Fahri MacBook <fahri-can@web.de>
2019-11-30 15:51:40 +0100

    Add an inject method for ListViewModel.

--- app/src/main/java/com/example/countriesapp/di/ApiComponent.kt
+++ app/src/main/java/com/example/countriesapp/di/ApiComponent.kt
@@ -1,10 +1,13 @@
 package com.example.countriesapp.di
 
 import com.example.countriesapp.model.CountriesService
+import com.example.countriesapp.viewmodel.ListViewModel
 import dagger.Component
 
 @Component(modules = [ApiModule::class])
 interface ApiComponent {
 
     fun inject(service: CountriesService)
+
+    fun inject(viewModel: ListViewModel)
 }
